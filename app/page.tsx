488292369db5e31d81944ec131083f1b11cdf9a5
NomanGul <nomangul2001@gmail.com>
2025-10-21 12:51:40 +0500

    fix height issues

--- app/page.tsx
+++ app/page.tsx
@@ -15,37 +15,31 @@ export default function Page() {
       <Header />
 
       <div className="px-6 pb-6 max-w-7xl mx-auto">
-        <Screen0 />
+        <div className="flex items-center justify-center customeHeight">
+          <Screen0 />
+        </div>
         <Layout />
-        <TestimonialSection />
-        <SignupUnitSection />
-        <PricingSection />
+        <div className="flex items-center justify-center customeHeight">
+          <TestimonialSection />
+        </div>
+        <div className="flex items-center justify-center customeHeight">
+          <SignupUnitSection />
+        </div>
+        <div className="flex items-center justify-center customeHeight">
+          <PricingSection />
+        </div>
         <PhilosophySection />
-        <InClassSection />
-      </div>
-      <div>
-        <div
-          className={`flex flex-col md:flex-row gap-4 items-center justify-center h-60  py-4 text-center  transition-opacity duration-700  "opacity-100"
-            }`}
-        >
-          <div className="flex-1 text-md border-t border-gray-500 pt-4">
-            <p className="text-gray-700 mb-4">“It felt like a dream...like having another teacher in my classroom”</p>
-            <p className="font-semibold">Mr. Peter Lu</p>
-            <p className=" text-gray-500">Mr. Peter Lu
-              Math Teacher at Hilltop High School | Stanford Alum </p>
-          </div>
-
-          <img
-            src="/boy.svg"
-            alt='boy'
-            className="w-40 h-40 object-fit"
-          />
+        <div className="flex items-center justify-center customeHeight">
+          <InClassSection />
         </div>
       </div>
+    
 
 
       <div className="max-w-7xl mx-auto">
-        <LearnWith />
+        <div className="flex items-center justify-center customeHeight">
+          <LearnWith />
+        </div>
       </div>
     </div>
   );
